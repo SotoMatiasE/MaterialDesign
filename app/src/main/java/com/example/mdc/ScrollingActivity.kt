80d571ed7c51972db48f4f6345fc1e86fa1f7dce
Matias <matias_r12@hotmail.com>
2024-05-21 16:57:11 -0300

    Configuracion de Glide para consumo de internet

--- app/src/main/java/com/example/mdc/ScrollingActivity.kt
+++ app/src/main/java/com/example/mdc/ScrollingActivity.kt
@@ -3,10 +3,13 @@ package com.example.mdc
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.widget.Toast
 import com.google.android.material.appbar.CollapsingToolbarLayout
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
 import com.example.mdc.databinding.ActivityScrollingBinding
 import com.google.android.material.bottomappbar.BottomAppBar
 
@@ -35,6 +38,31 @@ class ScrollingActivity : AppCompatActivity() {
                 .setAnchorView(binding.fab) //mueve el mensaje del snackbar por encima del navigation
                 .show()
         }
+
+        //dandole un id a include dentro de activity scroll podemos acceder a todos los botones de
+        //content_scrolling
+        binding.content.btnSkip.setOnClickListener{
+            binding.content.cvAdd.visibility = View.GONE
+        }
+
+        //Mensaje con accion con Toast
+        /*SnackBar depende de una sola vista, en cambio Toast nos sirve para cambiar de actividad
+        o de aplicacion porque es un mensaje que se muestra en la interfaz del sistema*/
+        binding.content.btnBuy.setOnClickListener{
+            Snackbar.make(it, R.string.card_buying, Snackbar.LENGTH_LONG)
+                .setAnchorView(binding.fab) //mueve el mensaje del snackbar por enc
+                .setAction(R.string.caard_to_go,{
+                    Toast.makeText(this, R.string.card_historial, Toast.LENGTH_SHORT).show()
+                })
+                .show()
+        }
+
+        /*descargar imágenes y guardarlas en caché para ser utilizadas
+        posteriormente por una aplicación Android*/
+        Glide.with(this)
+            .load("https://www.assemblyai.com/blog/content/images/2022/07/How-Imagen-Actually-Works.png")
+            //into ES PARA DARLE DONDE QUEREMOS QUE SE CARGE LA IMAGEN
+            .into(binding.content.imgCover)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
